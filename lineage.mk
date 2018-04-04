5da67308dd17f2560339404c1e63513d70c09be3
KaoTD <kaotd99@gmail.com>
2018-04-04 09:28:25 +0000

    lineage-15.1 bring up & use my repos

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,5 @@
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-
-# Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
@@ -10,9 +7,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/leo/leo:N/NPD56N/1000106:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="leo-user N NPD56N 1000106 release-keys"
 
-PRODUCT_NAME := carbon_z3
+PRODUCT_NAME := lineage_z3
 PRODUCT_DEVICE := z3
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="Myself5"
 
